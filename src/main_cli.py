d1c659a5c9bfcb432449520e641a0144ddb38d32
guglilac <x7deed53bc9k8@gmail.com>
2024-09-16 21:12:51 +0900

    fix

--- src/main_cli.py
+++ src/main_cli.py
@@ -1,4 +1,5 @@
-from block import Block, Board, Cell, Position
+from block import Board, Cell
+from samples import sample_map
 from solver import Solver
 from util import display_moves
 
@@ -7,40 +8,10 @@ def main():
     N = 6
     goal = Cell(x=5, y=2)
 
-    # init_positions = sample_map["pro_12"]
+    init_positions = sample_map["pro_12"]
 
-    # board = Board(width=N, height=N, goal=goal, positions=init_positions)
-    board = Board(
-        width=6,
-        height=6,
-        goal=Cell(y=2, x=5),
-        positions=[
-            Position(
-                cell=Cell(y=2, x=0),
-                block=Block(id=0, orientation="H", length=2, is_target=False),
-            ),
-            Position(
-                cell=Cell(y=0, x=4),
-                block=Block(id=1, orientation="H", length=2, is_target=False),
-            ),
-            Position(
-                cell=Cell(y=4, x=2),
-                block=Block(id=2, orientation="H", length=2, is_target=False),
-            ),
-            Position(
-                cell=Cell(y=3, x=1),
-                block=Block(id=3, orientation="V", length=2, is_target=False),
-            ),
-            Position(
-                cell=Cell(y=1, x=4),
-                block=Block(id=4, orientation="V", length=2, is_target=False),
-            ),
-            Position(
-                cell=Cell(y=3, x=4),
-                block=Block(id=5, orientation="V", length=2, is_target=False),
-            ),
-        ],
-    )
+    board = Board(width=N, height=N, goal=goal, positions=init_positions)
+
     solver = Solver()
     best_moves = solver.run(board=board)
 
